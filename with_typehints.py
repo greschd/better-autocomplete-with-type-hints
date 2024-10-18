6c1b8536e439faafdb574b3210c4f21607b4dec5
Dominik Gresch <dominik.gresch@ansys.com>
2024-10-18 12:02:07 +0200

    Add example with typehints

---
+++ with_typehints.py
@@ -0,0 +1,47 @@
+from collections.abc import Callable, Collection, Iterable, Iterator
+from typing import ParamSpec, TypeVar
+
+
+class MyObject:
+    def __init__(self, x: int, y: str, z: bool):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+def define_object_factory(object_type: Callable[P, T]) -> Callable[P, T]:
+    def inner(*args: P.args, **kwargs: P.kwargs) -> T:
+        return object_type(*args, **kwargs)
+
+    return inner
+
+
+class MyCollection(Collection[T]):
+    def __init__(self, values: Iterable[T]):
+        self._values = list(values)
+
+    def __contains__(self, value: object) -> bool:
+        return value in self._values
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self._values)
+
+    def __len__(self) -> int:
+        return len(self._values)
+
+
+def create_collection(values: Iterable[T] = ()) -> MyCollection[T]:
+    return MyCollection(values=values)
+
+
+create_myobject = define_object_factory(MyObject)
+
+create_myobject(1, "a", False)
+
+collection = create_collection(values=[MyObject(x=1, y="a", z=False)])
+
+obj = next(iter(collection))
